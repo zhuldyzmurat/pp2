66ec0218ff2b1989daf10c337ad0da6e9a47d252
zhuldyzmurat <156789758+zhuldyzmurat@users.noreply.github.com>
2024-02-06 17:10:11 +0600

    Add files via upload

---
+++ lab3/inheritances/inheritances.py
@@ -0,0 +1,17 @@
+#ex1
+class Student(Person):
+
+#ex2
+ class Person:
+  def __init__(self, fname):
+    self.firstname = fname
+
+  def printname(self):
+    print(self.firstname)
+
+class Student(Person):
+  pass
+
+x = Student("Mike")
+x.printname()
+
---
+++ lab3/modules/modules.py
@@ -0,0 +1,13 @@
+#ex1
+import mymodule
+
+#ex2
+import mymodule as mx
+
+#ex3
+import mymodule
+
+print(dir(mymodule))
+
+#ex4
+from mymodule import person1
